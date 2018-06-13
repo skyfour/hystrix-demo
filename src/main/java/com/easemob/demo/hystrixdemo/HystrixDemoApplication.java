3a4eaf5dbb0bd34984dc97b010e85fa9a9f07a0c
Strong Liu <stliu@easemob.com>
2018-06-13 22:34:14 +0800

    initial commit

---
+++ src/main/java/com/easemob/demo/hystrixdemo/HystrixDemoApplication.java
@@ -0,0 +1,12 @@
+package com.easemob.demo.hystrixdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDemoApplication.class, args);
+	}
+}
